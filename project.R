706792921d08023d2639f0147a27ea7ad11e3f78
yangh9596@gmail.com <yangh9596@126.com>
2016-04-30 14:26:50 +0800

    new file

---
+++ project.R
@@ -0,0 +1,8 @@
+# packages needed
+
+# input data
+setwd("/Users/yangh/Desktop/HKUST/FINA4414/Project")
+dat <- read.csv("prices.csv",
+                sep = ",",
+                header = TRUE,
+                stringsAsFactors = FALSE)

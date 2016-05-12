6dd3f8d63065f994606a54dcaa3c9d1fa79654d9
Yolanda <yangh9596@gmail.com>
2016-05-12 11:51:55 +0800

    Delete project.R

--- project.R
+++
@@ -1,9 +0,0 @@
-# packages needed
-
-# input data
-setwd("/Users/yangh/Desktop/HKUST/FINA4414/Project")
-dat <- read.csv("prices.csv",
-                sep = ",",
-                header = TRUE,
-                stringsAsFactors = FALSE)
-dat <- dat[,-1]
